9242d18fcdb59839042ecfecbbd11dd836e36510
Andrea del Rio <adelrio@gmail.com>
2018-06-05 10:14:54 -0700

    add package

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,23 +2,30 @@
 extern crate neon;
 extern crate fuzzy_phrase;
 
-use neon::vm::{Call, JsResult};
+use neon::vm::{Call, JsResult, FunctionCall, This};
+use neon::js::{Value};
 use neon::js::JsString;
 
-trait CheckArgument {
-  fn check_argument<V: Value>(&mut self, i: i32) -> JsResult<V>;
-}
+use fuzzy_phrase::{FuzzyPhraseSetBuilder};
 
-impl<'a, T: This> CheckArgument for FunctionCall<'a, T> {
-  fn check_argument<V: Value>(&mut self, i: i32) -> JsResult<V> {
-    self.arguments.require(self.scope, i)?.check::<V>()
-  }
-}
+// check argument type
+// trait CheckArgument {
+//   fn check_argument<V: Value>(&mut self, i: i32) -> JsResult<V>;
+// }
+//
+// impl<'a, T: This> CheckArgument for FunctionCall<'a, T> {
+//   fn check_argument<V: Value>(&mut self, i: i32) -> JsResult<V> {
+//     self.arguments.require(self.scope, i)?.check::<V>()
+//   }
+// }
 
 declare_types! {
-    // pub class JsFuzzyPhraseSetBuilder as JsFuzzyPhraseSetBuilder for Option<FuzzyPhraseSetBuilder<io::BufWriter<File>>>{
-    //
-    // }
+    pub class JsFuzzyPhraseSetBuilder as JsFuzzyPhraseSetBuilder for Option<FuzzyPhraseSetBuilder<P: AsRef<Path>>(path: P) -> Result<Self, Box<Error>>> {
+        init(mut call) {
+
+            Ok(())
+        }
+    }
 }
 
 register_module!(m, {
