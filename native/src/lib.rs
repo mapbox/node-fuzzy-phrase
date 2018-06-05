53141db7f97d792594b341f7a50426c33923dec7
Andrea del Rio <adelrio@gmail.com>
2018-06-05 16:33:56 -0700

    update creates, add traits

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,13 +2,16 @@
 extern crate neon;
 extern crate fuzzy_phrase;
 
-use neon::vm::{Call, JsResult, FunctionCall, This};
+use std::path::{Path};
+use std::string::String;
+
+use neon::vm::{JsResult, FunctionCall, This};
 use neon::js::{Value};
 use neon::js::JsString;
 
 use fuzzy_phrase::glue::{FuzzyPhraseSetBuilder, FuzzyPhraseSet};
 
-// check argument type
+// check argument type: https://github.com/Brooooooklyn/sourcemap-decoder/blob/master/native/src/lib.rs#L21-L29
 trait CheckArgument {
   fn check_argument<V: Value>(&mut self, i: i32) -> JsResult<V>;
 }
@@ -19,16 +22,17 @@ impl<'a, T: This> CheckArgument for FunctionCall<'a, T> {
   }
 }
 
+impl AsRef<Path> for String {}
 // FuzzyPhraseSetBuilder::new!(fuzzy_phrase::FuzzyPhraseSetBuilder);
 
 declare_types! {
-    pub class JsFuzzyPhraseSetBuilder as JsFuzzyPhraseSetBuilder for Option<FuzzyPhraseSetBuilder<std::string::String>> {
+    pub class JsFuzzyPhraseSetBuilder as JsFuzzyPhraseSetBuilder for Option<FuzzyPhraseSetBuilder<std::string::String<String>>> {
         init(mut call) {
             let filename = call
                 .check_argument::<JsString>(0)
                 ?.value();
-            let wtr = String::new(P: AsRef<Path>(filename).unwrap());
-            let mut build = FuzzyPhraseSetBuilder::new(wtr).unwrap();
+            let path = filename.as_ref().unwrap();
+            let mut build = FuzzyPhraseSetBuilder::new(path).unwrap();
             Ok(Some(build))
         }
     }
