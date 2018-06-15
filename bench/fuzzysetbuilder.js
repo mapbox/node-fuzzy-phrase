7b080ed85cd11771efa738eb5f40fa68fad485b3
Andrea del Rio <adelrio@gmail.com>
2018-06-15 16:34:11 -0700

    addd benchmarking notes

--- bench/fuzzysetbuilder.js
+++ bench/fuzzysetbuilder.js
@@ -5,6 +5,38 @@ let suite = new require('benchmark').Suite();
 
 module.exports = setup;
 
+// var totalTime;
+// var start = new Date;
+// var iterations = 6;
+// while (iterations--) {
+// 	// Code snippet goes here.
+// }
+// // `totalTime` is the number of milliseconds it took to execute the code snippet 6 times.
+// totalTime = new Date - start;
+// 
+// OR
+// var hz;
+// var period;
+// var startTime = new Date;
+// var runs = 0;
+// do {
+// 	// Code snippet goes here.
+// 	runs++;
+// 	totalTime = new Date - startTime;
+// } while (totalTime < 1000);
+//
+// // Convert milliseconds to seconds.
+// totalTime /= 1000;
+//
+// // period → how long each operation takes
+// period = totalTime / runs;
+//
+// // hz → the number of operations per second.
+// hz = 1 / period;
+//
+// // This can be shortened to:
+// // hz = (runs * 1000) / totalTime;
+
 function setup(cb) {
     if (!cb) cb = function(){};
     console.log('# FuzzyPhraseSetBuilder');
