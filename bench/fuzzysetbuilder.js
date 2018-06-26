55912ff5fd0a2aba2d6aec28d14c8aaa53651552
Andrea del Rio <adelrio@gmail.com>
2018-06-26 15:46:41 -0700

    adjust benches

--- bench/fuzzysetbuilder.js
+++ bench/fuzzysetbuilder.js
@@ -14,11 +14,31 @@ let containsPrefixTotalTime = 0;
 let fuzzyMatchTotalTime = 0;
 let fuzzyMatchPrefixTotalTime = 0;
 
+// check if test data is available
 if (!(fs.existsSync('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt'))) {
     console.error('     Please run `yarn bench`');
 }
 
-console.log("benching");
+// need to put everything in an array before doing a lookup
+console.log("setting up... ");
+let docs = fs.createReadStream('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt');
+let rl = readline.createInterface({
+    input: docs
+});
+
+// create some arrays
+let phraseSetArray = [];
+rl.on('line', (line) => {
+    phraseArray = []
+    line.split(" ").forEach((word) => {
+        phraseArray.push(word);
+    })
+    phraseSetArray.push(phraseArray);
+})
+
+rl.close()
+
+console.log("benching...");
 
 while (iterations >= 0) {
     iterations -= 1;
@@ -28,87 +48,38 @@ while (iterations >= 0) {
     let startTime = new Date;
     let setBuilder = new fuzzy.FuzzyPhraseSetBuilder("bench.fuzzy")
 
-    // check if test data is available
-
-    let docs = fs.createReadStream('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt');
-    let rl = readline.createInterface({
-        input: docs
-    });
-
-    rl.on('line', (line) => {
-        console.log(line);
-        setBuilder.insert(line);
+    // iterate over phraseSetArray to check for each phrase.
+    phraseSetArray.forEach((phrase) => {
+        setBuilder.insert(phrase);
     })
-
-    rl.close()
     setBuilder.finish();
     setBuildTotalTime += (new Date - startTime);
 
 
     // FuzzyPhraseSet lookup bench
-    // FuzzyPhraseSet.contains() bench
-    startTime = new Date;
     let set = new fuzzy.FuzzyPhraseSet("bench.fuzzy");
-    docs = fs.createReadStream('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt');
-    rl = readline.createInterface({
-        input: docs
-    });
+    for (var i = 0; i < phraseSetArray.length; i++) {
 
-    rl.on('line', (line) => {
-        console.log(line);
-        set.contains(line);
-    })
+        // FuzzyPhraseSet.contains() bench
+        startTime = new Date;
+        set.contains(phraseSetArray[i]);
+        containsTotalTime += (new Date - startTime);
 
-    rl.close()
-    containsTotalTime += (new Date - startTime);
+        // FuzzyPhraseSet.contains_prefix() bench
+        startTime = new Date;
+        set.contains_prefix(phraseSetArray[i]);
+        containsPrefixTotalTime += (new Date - startTime);
 
-    // FuzzyPhraseSet.contains_prefix() bench
-    startTime = new Date;
-    set = new fuzzy.FuzzyPhraseSet("bench.fuzzy");
-    docs = fs.createReadStream('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt');
-    rl = readline.createInterface({
-        input: docs
-    });
+        // FuzzyPhraseSet.fuzzy_match() bench
+        startTime = new Date;
+        set.fuzzy_match(phraseSetArray[i], 1, 1);
+        fuzzyMatchTotalTime += (new Date - startTime);
 
-    rl.on('line', (line) => {
-        console.log(line);
-        set.contains_prefix(line);
-    })
-
-    rl.close()
-    containsPrefixTotalTime += (new Date - startTime);
-
-    // FuzzyPhraseSet.fuzzy_match() bench
-    startTime = new Date;
-    set = new fuzzy.FuzzyPhraseSet("bench.fuzzy");
-    docs = fs.createReadStream('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt');
-    rl = readline.createInterface({
-        input: docs
-    });
-
-    rl.on('line', (line) => {
-        console.log(line);
-        set.fuzzy_match(line, 1, 1);
-    })
-
-    rl.close()
-    fuzzyMatchTotalTime += (new Date - startTime);
-
-    // FuzzyPhraseSet.fuzzy_match_prefix() bench
-    startTime = new Date;
-    set = new fuzzy.FuzzyPhraseSet("bench.fuzzy");
-    docs = fs.createReadStream('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt');
-    rl = readline.createInterface({
-        input: docs
-    });
-
-    rl.on('line', (line) => {
-        console.log(line);
-        set.fuzzy_match_prefix(line, 1, 1);
-    })
-
-    rl.close()
-    fuzzyMatchPrefixTotalTime += (new Date - startTime);
+        // FuzzyPhraseSet.fuzzy_match_prefix() bench
+        startTime = new Date;
+        set.fuzzy_match_prefix(phraseSetArray[i], 1, 1);
+        fuzzyMatchPrefixTotalTime += (new Date - startTime);
+    }
 }
 
 console.log("# FuzzyPhraseSetBuilder build: ");
