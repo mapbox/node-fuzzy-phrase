851ce1bc86090687fd9fc749c86938571f414ebd
Andrea del Rio <adelrio@gmail.com>
2018-06-27 10:35:37 -0700

    mv benches to after rl closes

--- bench/fuzzysetbuilder.js
+++ bench/fuzzysetbuilder.js
@@ -24,7 +24,7 @@ console.log("setting up... ");
 let docs = fs.createReadStream('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt');
 let rl = readline.createInterface({
     input: docs,
-    terminal: false
+    output: process.stdout,
 });
 
 // create some arrays
@@ -33,18 +33,14 @@ rl.on('line', (line) => {
     phraseArray = []
     line.split(" ").forEach((word) => {
         word = word.toString();
-        console.log(`line 36: ${word}`);
+        // console.log(`line 36: ${word}`);
         phraseArray.push(word);
     })
     phraseSetArray.push(phraseArray);
     rl.close();
-})
-
-console.log("setup complete");
-
-console.log("benching...");
-
-rl.on('close', () => {
+    console.log("setup complete");
+}).on('close', () => {
+    console.log("benching...");
     while (iterations >= 0) {
         iterations -= 1;
 
@@ -60,45 +56,43 @@ rl.on('close', () => {
         setBuilder.finish();
         setBuildTotalTime += (new Date - startTime);
 
-        // rl.close();
-
-            let set = new fuzzy.FuzzyPhraseSet("bench.fuzzy");
-            for (let i = 0; i < phraseSetArray.length; i++) {
-
-                // FuzzyPhraseSet.contains() bench
-                startTime = new Date;
-                set.contains(phraseSetArray[i]);
-                containsTotalTime += (new Date - startTime);
-
-                // FuzzyPhraseSet.contains_prefix() bench
-                startTime = new Date;
-                set.contains_prefix(phraseSetArray[i]);
-                containsPrefixTotalTime += (new Date - startTime);
-
-                // FuzzyPhraseSet.fuzzy_match() bench
-                startTime = new Date;
-                set.fuzzy_match(phraseSetArray[i], 1, 1);
-                fuzzyMatchTotalTime += (new Date - startTime);
-
-                // FuzzyPhraseSet.fuzzy_match_prefix() bench
-                startTime = new Date;
-                set.fuzzy_match_prefix(phraseSetArray[i], 1, 1);
-                fuzzyMatchPrefixTotalTime += (new Date - startTime);
-            }
         // FuzzyPhraseSet lookup bench
+        let set = new fuzzy.FuzzyPhraseSet("bench.fuzzy");
+        for (let i = 0; i < phraseSetArray.length; i++) {
+
+            // FuzzyPhraseSet.contains() bench
+            startTime = new Date;
+            set.contains(phraseSetArray[i]);
+            containsTotalTime += (new Date - startTime);
+
+            // FuzzyPhraseSet.contains_prefix() bench
+            startTime = new Date;
+            set.contains_prefix(phraseSetArray[i]);
+            containsPrefixTotalTime += (new Date - startTime);
+
+            // FuzzyPhraseSet.fuzzy_match() bench
+            startTime = new Date;
+            set.fuzzy_match(phraseSetArray[i], 1, 1);
+            fuzzyMatchTotalTime += (new Date - startTime);
+
+            // FuzzyPhraseSet.fuzzy_match_prefix() bench
+            startTime = new Date;
+            set.fuzzy_match_prefix(phraseSetArray[i], 1, 1);
+            fuzzyMatchPrefixTotalTime += (new Date - startTime);
+        }
     }
+    rl.output.write("This is the end");
+    rl.close()
+    console.log("benching complete");
+    console.log(" ");
+
+    console.log("Benchmark results: ");
+    console.log("# FuzzyPhraseSetBuilder build: ");
+    console.log('     avg FuzzyPhraseSetBuilder setup time: ' + (setBuildTotalTime/1000) + 'ms');
+
+    console.log("# FuzzyPhraseSet lookup");
+    console.log('     avg contains() setup time: ' + (containsTotalTime/1000) + 'ms');
+    console.log('     avg contains_prefix() lookup time: ' + (containsPrefixTotalTime/1000) + 'ms');
+    console.log('     avg fuzzy_match() lookup time: ' + (fuzzyMatchTotalTime/1000) + 'ms');
+    console.log('     avg fuzzy_match_prefix() lookup time: ' + (fuzzyMatchPrefixTotalTime/1000) + 'ms');
 })
-// rl.close()
-
-console.log("benching complete");
-console.log(" ");
-
-console.log("Benchmark results: ");
-console.log("# FuzzyPhraseSetBuilder build: ");
-console.log('     avg FuzzyPhraseSetBuilder setup time: ' + (setBuildTotalTime/1000) + 'ms');
-
-console.log("# FuzzyPhraseSet lookup");
-console.log('     avg contains() setup time: ' + (containsTotalTime/1000) + 'ms');
-console.log('     avg contains_prefix() lookup time: ' + (containsPrefixTotalTime/1000) + 'ms');
-console.log('     avg fuzzy_match() lookup time: ' + (fuzzyMatchTotalTime/1000) + 'ms');
-console.log('     avg fuzzy_match_prefix() lookup time: ' + (fuzzyMatchPrefixTotalTime/1000) + 'ms');
