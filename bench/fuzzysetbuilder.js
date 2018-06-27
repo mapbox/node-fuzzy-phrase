7bbe0e6234e350d384dc908a224a5c81b9cf0d72
Andrea del Rio <adelrio@gmail.com>
2018-06-27 10:39:41 -0700

    rm inline comments

--- bench/fuzzysetbuilder.js
+++ bench/fuzzysetbuilder.js
@@ -14,12 +14,10 @@ let containsPrefixTotalTime = 0;
 let fuzzyMatchTotalTime = 0;
 let fuzzyMatchPrefixTotalTime = 0;
 
-// check if test data is available
 if (!(fs.existsSync('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt'))) {
     console.error('     Please run `yarn bench`');
 }
 
-// need to put everything in an array before doing a lookup
 console.log("setting up... ");
 let docs = fs.createReadStream('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt');
 let rl = readline.createInterface({
@@ -27,13 +25,11 @@ let rl = readline.createInterface({
     output: process.stdout,
 });
 
-// create some arrays
 let phraseSetArray = [];
 rl.on('line', (line) => {
     phraseArray = []
     line.split(" ").forEach((word) => {
         word = word.toString();
-        // console.log(`line 36: ${word}`);
         phraseArray.push(word);
     })
     phraseSetArray.push(phraseArray);
@@ -44,44 +40,35 @@ rl.on('line', (line) => {
     while (iterations >= 0) {
         iterations -= 1;
 
-        // # FuzzyPhraseSetBuilder bench
-        //  build set
         let startTime = new Date;
         let setBuilder = new fuzzy.FuzzyPhraseSetBuilder("bench.fuzzy")
 
-        // iterate over phraseSetArray to check for each phrase.
         phraseSetArray.forEach((phrase) => {
             setBuilder.insert(phrase);
         })
         setBuilder.finish();
         setBuildTotalTime += (new Date - startTime);
 
-        // FuzzyPhraseSet lookup bench
         let set = new fuzzy.FuzzyPhraseSet("bench.fuzzy");
         for (let i = 0; i < phraseSetArray.length; i++) {
 
-            // FuzzyPhraseSet.contains() bench
             startTime = new Date;
             set.contains(phraseSetArray[i]);
             containsTotalTime += (new Date - startTime);
 
-            // FuzzyPhraseSet.contains_prefix() bench
             startTime = new Date;
             set.contains_prefix(phraseSetArray[i]);
             containsPrefixTotalTime += (new Date - startTime);
 
-            // FuzzyPhraseSet.fuzzy_match() bench
             startTime = new Date;
             set.fuzzy_match(phraseSetArray[i], 1, 1);
             fuzzyMatchTotalTime += (new Date - startTime);
 
-            // FuzzyPhraseSet.fuzzy_match_prefix() bench
             startTime = new Date;
             set.fuzzy_match_prefix(phraseSetArray[i], 1, 1);
             fuzzyMatchPrefixTotalTime += (new Date - startTime);
         }
     }
-    rl.output.write("This is the end");
     rl.close()
     console.log("benching complete");
     console.log(" ");
