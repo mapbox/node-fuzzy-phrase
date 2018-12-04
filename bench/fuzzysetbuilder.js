ac289494626d07913afa8997bbb6a9f5221ffb79
Amila Welihinda <amilajack@gmail.com>
2018-12-04 13:42:15 -0800

    swap eslint-config-bliss for eslint-config-geocoding

--- bench/fuzzysetbuilder.js
+++ bench/fuzzysetbuilder.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require('fs');
 const readline = require('readline');
 const tmp = require('tmp');
@@ -13,15 +15,15 @@ let fuzzyMatchTotalTime = 0;
 let fuzzyMatchPrefixTotalTime = 0;
 
 if (!fs.existsSync('/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt')) {
-  console.error('     Please run `yarn bench`');
+    console.error('     Please run `yarn bench`');
 }
 
 console.log('setting up... ');
 const docs = fs.createReadStream(
-  '/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt'
+    '/tmp/fuzzy-phrase-bench/phrase/us_en_latn.txt'
 );
 const rl = readline.createInterface({
-  input: docs
+    input: docs
 });
 
 let sampleSize = 100000;
@@ -29,72 +31,72 @@ const phraseSetArray = [];
 let startTime = new Date();
 const setBuilder = new fuzzy.FuzzyPhraseSetBuilder(tmpDir.name);
 
-rl.on('line', line => {
-  const words = line.split(' ');
-  setBuilder.insert(words);
-  if (phraseSetArray.length < sampleSize && Math.random() < 0.01) {
-    phraseSetArray.push(words);
-  }
+rl.on('line', (line) => {
+    const words = line.split(' ');
+    setBuilder.insert(words);
+    if (phraseSetArray.length < sampleSize && Math.random() < 0.01) {
+        phraseSetArray.push(words);
+    }
 }).on('close', () => {
-  setBuilder.finish();
-  setBuildTotalTime += new Date() - startTime;
+    setBuilder.finish();
+    setBuildTotalTime += new Date() - startTime;
 
-  console.log('setup complete');
-  console.log('benching...');
+    console.log('setup complete');
+    console.log('benching...');
 
-  sampleSize = phraseSetArray.length;
+    sampleSize = phraseSetArray.length;
 
-  const set = new fuzzy.FuzzyPhraseSet(tmpDir.name);
-  startTime = new Date();
-  for (let i = 0; i < phraseSetArray.length; i++) {
-    set.contains(phraseSetArray[i]);
-  }
-  containsTotalTime = new Date() - startTime;
+    const set = new fuzzy.FuzzyPhraseSet(tmpDir.name);
+    startTime = new Date();
+    for (let i = 0; i < phraseSetArray.length; i++) {
+        set.contains(phraseSetArray[i]);
+    }
+    containsTotalTime = new Date() - startTime;
 
-  startTime = new Date();
-  for (let i = 0; i < phraseSetArray.length; i++) {
-    set.contains_prefix(phraseSetArray[i]);
-  }
-  containsPrefixTotalTime = new Date() - startTime;
+    startTime = new Date();
+    for (let i = 0; i < phraseSetArray.length; i++) {
+        set.contains_prefix(phraseSetArray[i]);
+    }
+    containsPrefixTotalTime = new Date() - startTime;
 
-  startTime = new Date();
-  for (let i = 0; i < phraseSetArray.length; i++) {
-    set.fuzzy_match(phraseSetArray[i], 1, 1);
-  }
-  fuzzyMatchTotalTime = new Date() - startTime;
+    startTime = new Date();
+    for (let i = 0; i < phraseSetArray.length; i++) {
+        set.fuzzy_match(phraseSetArray[i], 1, 1);
+    }
+    fuzzyMatchTotalTime = new Date() - startTime;
 
-  startTime = new Date();
-  for (let i = 0; i < phraseSetArray.length; i++) {
-    set.fuzzy_match_prefix(phraseSetArray[i], 1, 1);
-  }
-  fuzzyMatchPrefixTotalTime = new Date() - startTime;
+    startTime = new Date();
+    for (let i = 0; i < phraseSetArray.length; i++) {
+        set.fuzzy_match_prefix(phraseSetArray[i], 1, 1);
+    }
+    fuzzyMatchPrefixTotalTime = new Date() - startTime;
 
-  console.log(' ');
+    console.log(' ');
 
-  console.log('Benchmark results: ');
-  console.log('# FuzzyPhraseSetBuilder build: ');
-  console.log(
-    `     avg FuzzyPhraseSetBuilder setup time: ${setBuildTotalTime /
+    console.log('Benchmark results: ');
+    console.log('# FuzzyPhraseSetBuilder build: ');
+    console.log(
+        `     avg FuzzyPhraseSetBuilder setup time: ${setBuildTotalTime /
       sampleSize}ms`
-  );
+    );
 
-  console.log('# FuzzyPhraseSet lookup');
-  console.log(
-    `     avg contains() lookup time: ${containsTotalTime / sampleSize}ms`
-  );
-  console.log(
-    `     avg contains_prefix() lookup time: ${containsPrefixTotalTime /
+    console.log('# FuzzyPhraseSet lookup');
+    console.log(
+        `     avg contains() lookup time: ${containsTotalTime / sampleSize}ms`
+    );
+    console.log(
+        `     avg contains_prefix() lookup time: ${containsPrefixTotalTime /
       sampleSize}ms`
-  );
-  console.log(
-    `     avg fuzzy_match() lookup time: ${fuzzyMatchTotalTime / sampleSize}ms`
-  );
-  console.log(
-    `     avg fuzzy_match_prefix() lookup time: ${fuzzyMatchPrefixTotalTime /
+    );
+    console.log(
+        `     avg fuzzy_match() lookup time: ${fuzzyMatchTotalTime / sampleSize}ms`
+    );
+    console.log(
+        `     avg fuzzy_match_prefix() lookup time: ${fuzzyMatchPrefixTotalTime /
       sampleSize}ms`
-  );
+    );
 
-  rimraf(tmpDir.name);
+    rimraf(tmpDir.name);
 
-  process.exit(0);
+    process.exit(0);
 });
--- test/all.js
+++ test/all.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const tape = require('tape');
 const tmp = require('tmp');
 const rimraf = require('rimraf').sync;
@@ -5,199 +7,199 @@ const fuzzy = require('../lib');
 
 const tmpDir = tmp.dirSync();
 
-tape('build FuzzyPhraseSetBuilder', t => {
-  const builder = new fuzzy.FuzzyPhraseSetBuilder(tmpDir.name);
-  t.ok(builder, 'FuzzyPhraseSetBuilder built');
-  t.throws(() => {
-    new fuzzy.FuzzyPhraseSetBuilder();
-  }, 'throws on not enough arguments');
-  t.throws(() => {
-    new fuzzy.FuzzyPhraseSetBuilder('/etc/passwd');
-  }, 'throws on not a directory');
-  t.throws(() => {
-    new fuzzy.FuzzyPhraseSetBuilder({});
-  }, 'throws on wrong type argument');
-  t.throws(() => {
-    new fuzzy.FuzzyPhraseSetBuilder(7);
-  }, 'throws on wrong type arguments');
-  t.end();
+tape('build FuzzyPhraseSetBuilder', (t) => {
+    const builder = new fuzzy.FuzzyPhraseSetBuilder(tmpDir.name);
+    t.ok(builder, 'FuzzyPhraseSetBuilder built');
+    t.throws(() => {
+        new fuzzy.FuzzyPhraseSetBuilder();
+    }, 'throws on not enough arguments');
+    t.throws(() => {
+        new fuzzy.FuzzyPhraseSetBuilder('/etc/passwd');
+    }, 'throws on not a directory');
+    t.throws(() => {
+        new fuzzy.FuzzyPhraseSetBuilder({});
+    }, 'throws on wrong type argument');
+    t.throws(() => {
+        new fuzzy.FuzzyPhraseSetBuilder(7);
+    }, 'throws on wrong type arguments');
+    t.end();
 });
 
-tape('FuzzyPhraseSetBuilder insertion and Set lookup', t => {
-  const builder = new fuzzy.FuzzyPhraseSetBuilder(tmpDir.name);
+tape('FuzzyPhraseSetBuilder insertion and Set lookup', (t) => {
+    const builder = new fuzzy.FuzzyPhraseSetBuilder(tmpDir.name);
 
-  builder.insert(['100', 'main', 'street']);
-  builder.insert(['200', 'main', 'street']);
-  builder.insert(['100', 'main', 'ave']);
-  builder.insert(['300', 'mlk', 'blvd']);
-  builder.finish();
+    builder.insert(['100', 'main', 'street']);
+    builder.insert(['200', 'main', 'street']);
+    builder.insert(['100', 'main', 'ave']);
+    builder.insert(['300', 'mlk', 'blvd']);
+    builder.finish();
 
-  const set = new fuzzy.FuzzyPhraseSet(tmpDir.name);
-  t.ok(set.contains(['100', 'main', 'street']), 'FuzzyPhraseSet contains()');
-  t.ok(set.contains(['200', 'main', 'street']), 'FuzzyPhraseSet contains()');
-  t.ok(set.contains(['100', 'main', 'ave']), 'FuzzyPhraseSet contains()');
-  t.ok(set.contains(['300', 'mlk', 'blvd']), 'FuzzyPhraseSet contains()');
+    const set = new fuzzy.FuzzyPhraseSet(tmpDir.name);
+    t.ok(set.contains(['100', 'main', 'street']), 'FuzzyPhraseSet contains()');
+    t.ok(set.contains(['200', 'main', 'street']), 'FuzzyPhraseSet contains()');
+    t.ok(set.contains(['100', 'main', 'ave']), 'FuzzyPhraseSet contains()');
+    t.ok(set.contains(['300', 'mlk', 'blvd']), 'FuzzyPhraseSet contains()');
 
-  t.ok(set.contains(['100', 'main', 'street']), 'FuzzyPhraseSet contains()');
-  const phraseStatic = ['100', 'main', 'street'];
-  t.ok(set.contains(phraseStatic), 'FuzzyPhraseSet contains()');
-  const phraseVec = ['100', 'main', 'street'];
-  t.ok(set.contains(phraseVec), 'FuzzyPhraseSet contains()');
+    t.ok(set.contains(['100', 'main', 'street']), 'FuzzyPhraseSet contains()');
+    const phraseStatic = ['100', 'main', 'street'];
+    t.ok(set.contains(phraseStatic), 'FuzzyPhraseSet contains()');
+    const phraseVec = ['100', 'main', 'street'];
+    t.ok(set.contains(phraseVec), 'FuzzyPhraseSet contains()');
 
-  t.notOk(set.contains(['x']), 'FuzzyPhraseSet does not contains()');
-  t.notOk(set.contains(['100', 'main']), 'FuzzyPhraseSet does not contains()');
-  t.notOk(
-    set.contains(['100', 'main', 's']),
-    'FuzzyPhraseSet does not contains()'
-  );
-  t.notOk(
-    set.contains(['100', 'main', 'streetr']),
-    'FuzzyPhraseSet does not contains()'
-  );
-  t.notOk(
-    set.contains(['100', 'main', 'street', 'r']),
-    'FuzzyPhraseSet does not contains()'
-  );
-  t.notOk(
-    set.contains(['100', 'main', 'street', 'ave']),
-    'FuzzyPhraseSet does not contains()'
-  );
+    t.notOk(set.contains(['x']), 'FuzzyPhraseSet does not contains()');
+    t.notOk(set.contains(['100', 'main']), 'FuzzyPhraseSet does not contains()');
+    t.notOk(
+        set.contains(['100', 'main', 's']),
+        'FuzzyPhraseSet does not contains()'
+    );
+    t.notOk(
+        set.contains(['100', 'main', 'streetr']),
+        'FuzzyPhraseSet does not contains()'
+    );
+    t.notOk(
+        set.contains(['100', 'main', 'street', 'r']),
+        'FuzzyPhraseSet does not contains()'
+    );
+    t.notOk(
+        set.contains(['100', 'main', 'street', 'ave']),
+        'FuzzyPhraseSet does not contains()'
+    );
 
-  t.ok(
-    set.containsPrefix(['100', 'main', 'street']),
-    'FuzzyPhraseSet containsPrefix()'
-  );
-  t.ok(
-    set.containsPrefix(['200', 'main', 'street']),
-    'FuzzyPhraseSet containsPrefix()'
-  );
-  t.ok(
-    set.containsPrefix(['100', 'main', 'ave']),
-    'FuzzyPhraseSet containsPrefix()'
-  );
-  t.ok(
-    set.containsPrefix(['300', 'mlk', 'blvd']),
-    'FuzzyPhraseSet containsPrefix()'
-  );
+    t.ok(
+        set.containsPrefix(['100', 'main', 'street']),
+        'FuzzyPhraseSet containsPrefix()'
+    );
+    t.ok(
+        set.containsPrefix(['200', 'main', 'street']),
+        'FuzzyPhraseSet containsPrefix()'
+    );
+    t.ok(
+        set.containsPrefix(['100', 'main', 'ave']),
+        'FuzzyPhraseSet containsPrefix()'
+    );
+    t.ok(
+        set.containsPrefix(['300', 'mlk', 'blvd']),
+        'FuzzyPhraseSet containsPrefix()'
+    );
 
-  t.ok(set.containsPrefix(['100', 'main']), 'FuzzyPhraseSet containsPrefix()');
-  t.ok(set.containsPrefix(['200', 'main']), 'FuzzyPhraseSet containsPrefix()');
-  t.ok(set.containsPrefix(['100', 'main']), 'FuzzyPhraseSet containsPrefix()');
-  t.ok(set.containsPrefix(['300', 'mlk']), 'FuzzyPhraseSet containsPrefix()');
+    t.ok(set.containsPrefix(['100', 'main']), 'FuzzyPhraseSet containsPrefix()');
+    t.ok(set.containsPrefix(['200', 'main']), 'FuzzyPhraseSet containsPrefix()');
+    t.ok(set.containsPrefix(['100', 'main']), 'FuzzyPhraseSet containsPrefix()');
+    t.ok(set.containsPrefix(['300', 'mlk']), 'FuzzyPhraseSet containsPrefix()');
 
-  t.notOk(
-    set.containsPrefix(['100', 'man']),
-    'FuzzyPhraseSet does not containsPrefix()'
-  );
-  t.notOk(
-    set.containsPrefix(['400', 'main']),
-    'FuzzyPhraseSet does not containsPrefix()'
-  );
-  t.notOk(
-    set.containsPrefix(['100', 'main', 'street', 'x']),
-    'FuzzyPhraseSet does not containsPrefix()'
-  );
+    t.notOk(
+        set.containsPrefix(['100', 'man']),
+        'FuzzyPhraseSet does not containsPrefix()'
+    );
+    t.notOk(
+        set.containsPrefix(['400', 'main']),
+        'FuzzyPhraseSet does not containsPrefix()'
+    );
+    t.notOk(
+        set.containsPrefix(['100', 'main', 'street', 'x']),
+        'FuzzyPhraseSet does not containsPrefix()'
+    );
 
-  t.ok(
-    set.fuzzyMatch(['100', 'man', 'street'], 1, 1),
-    'FuzzyPhraseSet fuzzyMatch()'
-  );
+    t.ok(
+        set.fuzzyMatch(['100', 'man', 'street'], 1, 1),
+        'FuzzyPhraseSet fuzzyMatch()'
+    );
 
-  t.deepEquals(
-    set.fuzzyMatch(['100', 'man', 'street'], 1, 1),
-    [{ edit_distance: 1, phrase: ['100', 'main', 'street'] }],
-    'FuzzyPhraseSet fuzzyMatch()'
-  );
+    t.deepEquals(
+        set.fuzzyMatch(['100', 'man', 'street'], 1, 1),
+        [{ edit_distance: 1, phrase: ['100', 'main', 'street'] }],
+        'FuzzyPhraseSet fuzzyMatch()'
+    );
 
-  t.deepEquals(
-    set.fuzzyMatch(['100', 'man', 'stret'], 1, 2),
-    [{ edit_distance: 2, phrase: ['100', 'main', 'street'] }],
-    'FuzzyPhraseSet fuzzyMatch()'
-  );
+    t.deepEquals(
+        set.fuzzyMatch(['100', 'man', 'stret'], 1, 2),
+        [{ edit_distance: 2, phrase: ['100', 'main', 'street'] }],
+        'FuzzyPhraseSet fuzzyMatch()'
+    );
 
-  t.deepEquals(
-    set.fuzzyMatchPrefix(['100', 'man'], 1, 1),
-    [{ phrase: ['100', 'main'], edit_distance: 1 }],
-    'FuzzyPhraseSet fuzzyMatchPrefix()'
-  );
+    t.deepEquals(
+        set.fuzzyMatchPrefix(['100', 'man'], 1, 1),
+        [{ phrase: ['100', 'main'], edit_distance: 1 }],
+        'FuzzyPhraseSet fuzzyMatchPrefix()'
+    );
 
-  t.deepEquals(
-    set.fuzzyMatchPrefix(['100', 'man', 'str'], 1, 1),
-    [{ phrase: ['100', 'main', 'str'], edit_distance: 1 }],
-    'FuzzyPhraseSet fuzzyMatchPrefix()'
-  );
+    t.deepEquals(
+        set.fuzzyMatchPrefix(['100', 'man', 'str'], 1, 1),
+        [{ phrase: ['100', 'main', 'str'], edit_distance: 1 }],
+        'FuzzyPhraseSet fuzzyMatchPrefix()'
+    );
 
-  t.deepEquals(
-    set.fuzzyMatchMulti(
-      [
-        [['100', 'man', 'street'], false],
-        [['100', 'man', 'stret'], false],
-        [['100', 'man'], true],
-        [['100', 'man', 'str'], true]
-      ],
-      1,
-      2
-    ),
-    [
-      [{ edit_distance: 1, phrase: ['100', 'main', 'street'] }],
-      [{ edit_distance: 2, phrase: ['100', 'main', 'street'] }],
-      [{ phrase: ['100', 'main'], edit_distance: 1 }],
-      [{ phrase: ['100', 'main', 'str'], edit_distance: 1 }]
-    ],
-    'FuzzyPhraseSet fuzzyMatchMulti()'
-  );
+    t.deepEquals(
+        set.fuzzyMatchMulti(
+            [
+                [['100', 'man', 'street'], false],
+                [['100', 'man', 'stret'], false],
+                [['100', 'man'], true],
+                [['100', 'man', 'str'], true]
+            ],
+            1,
+            2
+        ),
+        [
+            [{ edit_distance: 1, phrase: ['100', 'main', 'street'] }],
+            [{ edit_distance: 2, phrase: ['100', 'main', 'street'] }],
+            [{ phrase: ['100', 'main'], edit_distance: 1 }],
+            [{ phrase: ['100', 'main', 'str'], edit_distance: 1 }]
+        ],
+        'FuzzyPhraseSet fuzzyMatchMulti()'
+    );
 
-  t.deepEquals(
-    set.fuzzyMatchWindows(
-      '100 man street washington 200'.split(' '),
-      0,
-      0,
-      false
-    ),
-    []
-  );
+    t.deepEquals(
+        set.fuzzyMatchWindows(
+            '100 man street washington 200'.split(' '),
+            0,
+            0,
+            false
+        ),
+        []
+    );
 
-  t.deepEquals(
-    set.fuzzyMatchWindows(
-      '100 man street washington 200'.split(' '),
-      1,
-      1,
-      false
-    ),
-    [
-      {
-        edit_distance: 1,
-        ends_in_prefix: false,
-        phrase: ['100', 'main', 'street'],
-        start_position: 0
-      }
-    ]
-  );
+    t.deepEquals(
+        set.fuzzyMatchWindows(
+            '100 man street washington 200'.split(' '),
+            1,
+            1,
+            false
+        ),
+        [
+            {
+                edit_distance: 1,
+                ends_in_prefix: false,
+                phrase: ['100', 'main', 'street'],
+                start_position: 0
+            }
+        ]
+    );
 
-  t.deepEquals(
-    set.fuzzyMatchWindows(
-      '100 man street washington 200'.split(' '),
-      1,
-      1,
-      true
-    ),
-    [
-      {
-        edit_distance: 1,
-        ends_in_prefix: false,
-        phrase: ['100', 'main', 'street'],
-        start_position: 0
-      },
-      {
-        edit_distance: 0,
-        ends_in_prefix: true,
-        phrase: ['200'],
-        start_position: 4
-      }
-    ]
-  );
+    t.deepEquals(
+        set.fuzzyMatchWindows(
+            '100 man street washington 200'.split(' '),
+            1,
+            1,
+            true
+        ),
+        [
+            {
+                edit_distance: 1,
+                ends_in_prefix: false,
+                phrase: ['100', 'main', 'street'],
+                start_position: 0
+            },
+            {
+                edit_distance: 0,
+                ends_in_prefix: true,
+                phrase: ['200'],
+                start_position: 4
+            }
+        ]
+    );
 
-  rimraf(tmpDir.name);
+    rimraf(tmpDir.name);
 
-  t.end();
+    t.end();
 });
